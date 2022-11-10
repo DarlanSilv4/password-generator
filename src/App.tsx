42161d76916002acc4f76ab5115adb0f22c7c785
Darlan <darlan.silvadacruz@gmail.com>
2022-11-10 11:02:37 -0300

    (App.tsx) set up generate password button

--- src/App.tsx
+++ src/App.tsx
@@ -8,19 +8,48 @@ import CharacterOptions from "./components/CharacterOptions";
 import StrengthBox from "./components/StrengthBox";
 
 function App() {
+  const UPPERCASE_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const LOWERCASE_CHARS = "abcdefghijklmnopqrstuvwxyz";
+  const NUMBERS = "1234567890";
+  const SYMBOLS = " ~`!@#$%^&*()_-+={[}]|\\:;\"'<,>.?/";
+
   const [characterLength, setCharacterLength] = useState("10");
   const [checkboxOptions, setCheckboxOptions] = useState<string[]>([
     "uppercase",
     "lowercase",
     "numbers",
   ]);
+  const [password, setPassword] = useState("");
+
+  const handleGeneratePassword = () => {
+    const characters: string[] = [];
+    const passwordLength = parseInt(characterLength);
+
+    if (checkboxOptions.includes("uppercase")) characters.push(UPPERCASE_CHARS);
+    if (checkboxOptions.includes("lowercase")) characters.push(LOWERCASE_CHARS);
+    if (checkboxOptions.includes("numbers")) characters.push(NUMBERS);
+    if (checkboxOptions.includes("symbols")) characters.push(SYMBOLS);
+
+    const charactersInString = characters.join("");
+    const charactersLength = charactersInString.length;
+
+    const password: string[] = [];
+    for (let i = 0; i < passwordLength; i++) {
+      const radomChar = charactersInString.charAt(
+        Math.floor(Math.random() * charactersLength)
+      );
+
+      password.push(radomChar);
+    }
+
+    setPassword(password.join(""));
+  };
 
   return (
     <div className="flex flex-col gap-4 items-center justify-center min-w-screen min-h-screen py-5 bg-background text-white font-mono">
       <h1 className="text-white opacity-60 font-bold">Password Generator</h1>
       <section className="flex items-center justify-between p-4 bg-foreground w-11/12  sm:w-2/3 lg:w-2/5 lg:max-w-md text-white font-bold text-xl">
-        <p>OojJF57LtxT08AW53TBy</p>
-        <button className="cursor-pointer">
+        <p>{password}</p>
           <CopySvg className="text-green hover:text-white" />
         </button>
       </section>
@@ -37,7 +66,10 @@ function App() {
           charactersLength={characterLength}
           checkedOptions={checkboxOptions}
         />
-        <button className="flex p-4 gap-4 items-center justify-center w-full bg-green hover:bg-transparent text-foreground hover:text-green border border-green font-bold uppercase">
+        <button
+          className="flex p-4 gap-4 items-center justify-center w-full bg-green hover:bg-transparent text-foreground hover:text-green border border-green font-bold uppercase"
+          onClick={() => handleGeneratePassword()}
+        >
           <span>Generate</span>
           <ArrowSvg />
         </button>
